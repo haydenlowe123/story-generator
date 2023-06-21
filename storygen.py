22c786a3b0a33ed25726179a8686f548c19c7ba3
haydenlowe123 <137237140+haydenlowe123@users.noreply.github.com>
2023-06-21 01:11:28 +0100

    Add files via upload

---
+++ storygen.py
@@ -0,0 +1,21 @@
+# This is a short story generator 
+
+# The below section focuses on requesting information from the user via the input function, the input function requires a user action.
+
+noun = input('Choose a noun: ') 
+place = input('Name a place: ')
+adjective = input('Choose an adjective: ')
+
+# The below section is how the information taken from the user is actioned.
+
+print('------------------------------------------') #aesthetic
+
+print('The barclays employee was called ', noun, ) # this line calls to action the noun which was declared earlier
+
+print(noun,' worked in a big team') # this line calls to action the noun which was declared earlier
+
+print('They worked in', place) # this line calls to action the noun which was declared earlier
+
+print('They loved their job because it was', adjective, '. \n') # this line calls to action the adjective which was declared earlier
+
+print('------------------------------------------') #aesthetic
